8e69dca5aeeade8a820e77a6f82755639d958872
Lucas ROMARY <lucas.romary@utbm.fr>
2022-02-23 08:48:47 +0100

    feather_stepper_simple

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <Stepper.h>
+
+// OK pour feather pour les deux moteurs.
+const int stepsPerRevolution = 400;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 19, 16, 21, 17);
+
+void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(60);
+  // initialize the serial port:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // step one revolution  in one direction:
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  // step one revolution in the other direction:
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+}
